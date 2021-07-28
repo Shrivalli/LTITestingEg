495ab6ce0f31595fc4f803d67b5f57275577e510
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2021-07-28 09:26:00 +0530

    Added Empl

---
+++ Empl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingEg
+{
+    class Empl
+    {
+        public void Accept()
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
